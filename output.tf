42c6f9b01a0c4270feda60d7dae50ea93bf4bbba
harsha-aws <hv147369@gmail.com>
2024-09-22 18:45:44 +0530

    terraform

---
+++ output.tf
@@ -0,0 +1,11 @@
+# output "private" {
+#   value       = aws_instance.terraform.private
+# }
+
+# output "public" {
+#   value       = aws_instance.terraform.public
+# }
+
+# output "ids" {
+#   value       = aws_instance.terraform.ids
+# }
